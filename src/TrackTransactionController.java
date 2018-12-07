c12ba1557bf24753003ea9a6b95eda79d0bb991d
xCito <Juanlantiguajr@gmail.com>
2018-12-07 17:35:16 -0500

    Removed unused imports

--- src/TrackTransactionController.java
+++ src/TrackTransactionController.java
@@ -1,10 +1,7 @@
 import java.time.LocalDate;
 import java.util.List;
 
-import javafx.scene.chart.LineChart;
-import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
